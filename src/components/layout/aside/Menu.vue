ac9e2e927297f107cc8368f409dca86ba5f08719
zhangh@myy.ltd <zhangh@myy.ltd>
2022-05-25 23:18:46 +0800

    路由设计 封装icon组件

--- src/components/layout/aside/Menu.vue
+++ src/components/layout/aside/Menu.vue
@@ -2,9 +2,7 @@
 	<el-menu default-active="2" background-color="#001529" text-color="#fff">
 		<el-sub-menu index="1">
 			<template #title>
-				<el-icon>
-					<Setting />
-				</el-icon>
+				<Icon Icon="Setting" />
 				<span>系统管理</span>
 			</template>
 			<el-menu-item index="1-1">用户管理</el-menu-item>
@@ -12,16 +10,17 @@
 			<el-menu-item index="1-3">角色管理</el-menu-item>
 		</el-sub-menu>
 		<el-menu-item index="2">
-			<el-icon>
-				<icon-menu />
-			</el-icon>
+			<Icon Icon="Menu" />
 			<span>商品管理</span>
 		</el-menu-item>
 	</el-menu>
 </template>
 
 <script setup>
-import { Menu as IconMenu, Setting } from '@element-plus/icons-vue'
+import Icon from '@/components/common/Icon'
+import {ref} from 'vue'
+
+let menus = ref([])
 </script>
 
 <style scoped>
