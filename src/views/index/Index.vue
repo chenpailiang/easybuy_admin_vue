4ec5904e3e76180ed670f0cb2b78548314e73ae0
zhang hang <zhangh@myy.ltd>
2022-06-29 15:12:53 +0800

    首页

--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,9 +1,9 @@
 <template>
-	<div class="index-wrapper">
+	<div>
 		<div class="title">
 			<img class="pan-thumb" src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif"
 				alt="">
-			<h1 >易买超市管理系统</h1>
+			<h1>易买超市管理系统</h1>
 		</div>
 		<div>
 			<img src="https://wpimg.wallstcn.com/0e03b7da-db9e-4819-ba10-9016ddfdaed3" class="emptyGif">
@@ -15,9 +15,6 @@
 </script>
 
 <style scoped>
-.index-wrapper {
-	background: rgb(227, 227, 227);
-}
 .pan-thumb {
 	display: block;
 	width: 10rem;
@@ -29,13 +26,10 @@
 	justify-content: flex-start;
 	margin-top: 1rem;
 }
-.text{
-	font-size: 18px;
-	font-weight: bold;
-}
 .emptyGif {
 	display: block;
 	margin: 0 auto;
 	width: 45%;
+	background-color: rgb(249 250 251);
 }
 </style>
